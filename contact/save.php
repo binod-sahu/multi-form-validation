f96f6a92680898e70405a961bf23b95f5294ef9c
Your Name <you@example.com>
2020-07-05 02:04:36 +0100

    initial commit

---
+++ contact/save.php
@@ -0,0 +1,55 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST)) {
+    $contacts = json_decode($_POST["json"]);
+    $contactsCount = count($contacts);
+    $contactsToWrite = "";
+    
+    for ($x = 0; $x < $contactsCount; $x++) {
+        if( name_validate($contacts[$x]->name) && email_validate($contacts[$x]->email) && phone_validate($contacts[$x]->phone) ) {
+            $contactsToWrite .= "\n=============================================\n";
+            $contactsToWrite .= "name: " . $contacts[$x]->name . "\n";
+            $contactsToWrite .= "email: " . $contacts[$x]->email . "\n";
+            $contactsToWrite .= "phone: " . $contacts[$x]->phone . "\n";
+            $contactsToWrite .= "=============================================\n";
+        }
+    }
+
+    if (!empty($contactsToWrite)) {
+        writeToFile($contactsToWrite);
+        echo "message: Contacts are written to file successfully!!!";
+    } else {
+        echo "message: Contacts are not written to file!!!";
+    }
+    
+}
+
+function name_validate($name) {
+    $pattern = '/^[A-Za-z ]+$/';
+    return test_input($name, $pattern);
+}
+
+function email_validate($email) {
+    $pattern = '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
+    return test_input($email, $pattern);
+}
+
+function phone_validate($phone) {
+    $pattern = '/^[0-9]+$/';
+    return test_input($phone, $pattern);
+}
+
+function test_input($data, $pattern) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+
+    $match = @preg_match($pattern, $data, $matches);
+    return $match;
+}
+
+function writeToFile($contactsToWrite) {
+    $file = fopen("contacts.txt", "w");
+    fwrite($file, $contactsToWrite);
+    fclose($file);
+}
+?>
---
+++ js/multi-form-handler.js
@@ -0,0 +1,73 @@
+class newUser {
+    constructor(name, email, phone) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    }
+  }
+class Users {
+
+    formTemplate = document.querySelector('.contact-form');
+    formsContainer = document.querySelector('.forms');
+    formsChildren = this.formsContainer.children;
+
+    addForm = function() {
+        const cloneTemplate = this.formTemplate.content.cloneNode(true);
+        if(this.formsChildren.length){
+            this.formsContainer.insertBefore(cloneTemplate, this.formsChildren[0]);
+        } else {
+            this.formsContainer.insertBefore(cloneTemplate, null);
+        }
+    }
+    
+    removeForm = function(event) {
+       event.parentElement.parentElement.remove();
+    }
+    
+    validateForms = function() {
+        Array.from(this.formsChildren).map(item => {
+            const cName = item.querySelector('[name=c-name]').value;
+            const cEmail = item.querySelector('[name=c-email]').value;
+            const cPhone = item.querySelector('[name=c-phone]').value;
+            cName ? item.querySelector('.c-error.c-name').innerHTML = this.validateName(cName) ? '' : 'Invalid Name' 
+                    : item.querySelector('.c-error.c-name').innerHTML = 'Please enter Name';
+            cEmail ? item.querySelector('.c-error.c-email').innerHTML = this.validateEmail(cEmail) ? '' : 'Invalid Email' 
+                    : item.querySelector('.c-error.c-email').innerHTML = 'Please enter email address ';        
+            cPhone ? item.querySelector('.c-error.c-phone').innerHTML = this.validatePhone(cPhone) ? '' : 'Invalid Phone Number' 
+                    : item.querySelector('.c-error.c-phone').innerHTML = 'Please enter email address ';
+        })
+    }
+    saveForms = function() {
+        const data = new FormData();
+        const jsonSaveData = Array.from(this.formsChildren)
+                .map(item => new newUser(item.querySelector('[name=c-name]').value, item.querySelector('[name=c-email]').value, item.querySelector('[name=c-phone]').value))
+        const test = jsonSaveData.map(item => item)
+        console.log(test)
+        data.append( "json", JSON.stringify( jsonSaveData ));
+        fetch("/php-save-contacts/save.php",
+        {
+            method: "POST",
+            body: data
+        })
+        .then(function(res){ console.log(res); return res.json(); })
+        .then(function(data){ alert( JSON.stringify( data ) ) })
+    }
+    
+    validateName = function(name) {
+        const re = /^[A-Za-z ]+$/;
+        return re.test(name);
+    }
+    
+    validateEmail = function(email) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+    
+    validatePhone = function(phone) {
+        const re = /^[0-9]+$/;
+        return re.test(phone);
+    }
+}
+    
+
+const user = new Users()
